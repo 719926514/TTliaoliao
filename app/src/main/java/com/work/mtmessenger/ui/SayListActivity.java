f95bda836b0ab611479d63c9b15bc369e6812123
9951214892 <3430399727@.com>
2020-01-09 15:53:26 +0800

    ceshi

--- app/src/main/java/com/work/mtmessenger/ui/SayListActivity.java
+++ app/src/main/java/com/work/mtmessenger/ui/SayListActivity.java
@@ -185,7 +185,7 @@ public class SayListActivity extends BaseActivity {
                         }
 
                     }
-                }, 1100);
+                }, 1000);
             }
 
             @Override
